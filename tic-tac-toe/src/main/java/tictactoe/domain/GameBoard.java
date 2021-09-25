5a87da12440c5cf108866fdf0f5ea53ee565a2d3
Toni Rämö <toni.ramo@helsinki.fi>
2021-09-25 19:31:50 +0300

    Create javaDoc

--- tic-tac-toe/src/main/java/tictactoe/domain/GameBoard.java
+++ tic-tac-toe/src/main/java/tictactoe/domain/GameBoard.java
@@ -6,35 +6,58 @@
 package tictactoe.domain;
 
 /**
- *
- * @author toniramo
+ * Tic-tac-toe game board maintaining the moves made during the game.
  */
 public class GameBoard {
 
-    private Player[][] boardStatus;
-
+    private Player[][] boardState;
+    
+    /**
+     * Creates empty square game board with chosen size, usually checked from rule book.
+     * @param boardSize width of game board
+     */
     public GameBoard(int boardSize) {
-        this.boardStatus = new Player[boardSize + 1][boardSize + 1];
+        this.boardState = new Player[boardSize + 1][boardSize + 1];
     }
 
-    public GameBoard(Player[][] boardStatus) {
-        this.boardStatus = boardStatus;
+    /**
+     * Create game board based on known state.
+     * @param state predefined state based on which game board is generated
+     */
+    public GameBoard(Player[][] state) {
+        this.boardState = state;
     }
 
+    /**
+     * Set move on game board.
+     * @param move player made move to be stored.
+     */
     public void setMove(Move move) {
-        boardStatus[move.getX()][move.getY()] = move.getPlayer();
+        boardState[move.getX()][move.getY()] = move.getPlayer();
     }
 
+    /**
+     * Get move from game board based on x,y coordinate.
+     * @param x x-coordinate of the move to be get
+     * @param y y-coordinate of the move to be get
+     * @return move if found, otherwise null
+     */
     public Move getMove(int x, int y) {
-        int n = boardStatus.length - 1;
+        int n = boardState.length - 1;
         if (x > 0 && x <= n && y > 0 && y <= n) {
-            if (boardStatus[x][y] != null) {
-                return new Move(boardStatus[x][y], x, y);
+            if (boardState[x][y] != null) {
+                return new Move(boardState[x][y], x, y);
             }
         }
         return null;
     }
 
+    /**
+     * Get an array of moves based on given arrays of x,y-coordinates
+     * @param x x-coordinates
+     * @param y y-coordinates
+     * @return array of moves if found, otherwise null;
+     */
     public Move[] getMoves(int[] x, int[] y) {
         int n = x.length;
         Move[] moves = new Move[n];
@@ -46,16 +69,24 @@ public class GameBoard {
         return moves;
     }
 
+    /**
+     * Gets game board size, that is width of the edges (number of tiles per edge).
+     * @return width of game board edge
+     */
     public int getSize() {
-        return this.boardStatus.length - 1;
+        return this.boardState.length - 1;
     }
 
+    /**
+     * Gets number of moves on board.
+     * @return number of played moves
+     */
     public int getNumberOfPlayedMoves() {
         int moves = 0;
         int n = this.getSize();
         for (int x = 1; x <= n; x++) {
             for (int y = 1; y <= n; y++) {
-                if (this.boardStatus[x][y] != null) {
+                if (this.boardState[x][y] != null) {
                     moves++;
                 }
             }
@@ -63,12 +94,16 @@ public class GameBoard {
         return moves;
     }
 
+    /**
+     * Get copy of this game board. 
+     * @return copy of this game board
+     */
     public GameBoard getCopy() {
-        int n = boardStatus.length - 1;
+        int n = boardState.length - 1;
         Player[][] board = new Player[n + 1][n + 1];
         for (int x = 1; x <= n; x++) {
             for (int y = 1; y <= n; y++) {
-                board[x][y] = this.boardStatus[x][y];
+                board[x][y] = this.boardState[x][y];
             }
         }
         return new GameBoard(board);
