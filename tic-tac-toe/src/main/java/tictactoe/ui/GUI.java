79cec3868071def38569ec69ab3f7ef3b3fc566d
Toni Rämö <toni.ramo@helsinki.fi>
2021-09-30 09:36:21 +0300

    Add turn based change listener and get AI's move

--- tic-tac-toe/src/main/java/tictactoe/ui/GUI.java
+++ tic-tac-toe/src/main/java/tictactoe/ui/GUI.java
@@ -1,8 +1,12 @@
 package tictactoe.ui;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,21 +19,33 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import tictactoe.ai.AI;
 import tictactoe.dao.InMemoryDao;
 import tictactoe.domain.GameService;
+import tictactoe.domain.Move;
 import tictactoe.domain.Player;
+import tictactoe.domain.Player.PlayerType;
+import tictactoe.domain.RuleBook;
 
-/** 
+/**
  * Graphical user interface for tic-tac-toe.
  */
 public class GUI extends Application {
 
     private GameService gameService;
+    private AI ai;
+    private SimpleIntegerProperty turn;
+    private GridPane gameBoard;
 
     @Override
     public void init() {
+        Player humanPlayer = new Player("X", Color.TOMATO, PlayerType.HUMAN);
+        Player aiPlayer = new Player("O", Color.STEELBLUE, PlayerType.AI);
+        RuleBook rules = new RuleBook(20, 5, new Player[]{humanPlayer, aiPlayer});
         this.gameService = new GameService(new InMemoryDao());
-        this.gameService.startNewGame();
+        this.ai = new AI(gameService, aiPlayer, true);
+        this.gameService.startNewGame(rules);
+
     }
 
     @Override
@@ -43,7 +59,7 @@ public class GUI extends Application {
         Label turnLabel = new Label("Turn: " + gameService.getCurrentPlayer().getMark());
         turnLabel.setPadding(new Insets(10, 10, 10, 10));
 
-        GridPane gameBoard = new GridPane();
+        this.gameBoard = new GridPane();
 
         topMenuLayout.getChildren().addAll(newGameButton, exitButton, turnLabel);
 
@@ -64,35 +80,46 @@ public class GUI extends Application {
                     int x = GridPane.getColumnIndex(stack);
                     int y = GridPane.getRowIndex(stack);
 
-                    Player player = gameService.getCurrentPlayer();
                     Label mark = new Label();
                     mark.setFont(Font.font(STYLESHEET_MODENA, FontWeight.BOLD, 25));
                     if (gameService.validMove(x, y)) {
-                        gameService.makeMove(x, y);
-                        mark.setText(player.getMark());
-                        mark.setTextFill(player.getMarkColor());
-                        turnLabel.setText("Turn: " + gameService.getCurrentPlayer().getMark());
-                        stack.getChildren().add(mark);
+                        makeMove(x, y, stack, turnLabel);
                     }
 
-                    if (gameService.gameOver()) {
-                        String gameOverText;
-                        Player winner = gameService.getWinningPlayer();
-                        if (gameService.getWinningPlayer() != null) {
-                            gameOverText = " Winner: " + winner.getMark();
-                        } else {
-                            gameOverText = " Draw";
-                        }
-
-                        turnLabel.setText("Game over - " + gameOverText);
-                        //Turn of actions on game board.
-                        gameBoard.getChildren().forEach((node) -> {
-                            node.setOnMouseClicked(null);
-                        });
-                    }
                 });
             }
         }
+        turn = new SimpleIntegerProperty(gameService.getTurn());
+        turn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue ov, Object oldValue, Object newValue) {
+                if (gameService.getCurrentPlayer().equals(ai.getPlayer())) {
+                    Move move = ai.chooseMove(); // TODO implement waiting for AI to complete analysis
+                    for (Node node : gameBoard.getChildren()) {
+                        if (GridPane.getColumnIndex(node) == move.getX()
+                                && GridPane.getRowIndex(node) == move.getY()) {
+                            makeMove(move.getX(), move.getY(), (StackPane) node, turnLabel);
+                        }
+                    }
+                }
+                if (gameService.gameOver()) {
+                    String gameOverText;
+                    Player winner = gameService.getWinningPlayer();
+                    if (gameService.getWinningPlayer() != null) {
+                        gameOverText = " Winner: " + winner.getMark();
+                    } else {
+                        gameOverText = " Draw";
+                    }
+
+                    turnLabel.setText("Game over - " + gameOverText);
+                    //Turn off actions on game board.
+                    gameBoard.getChildren().forEach((node) -> {
+                        node.setOnMouseClicked(null);
+                    });
+                }
+            }
+        }
+        );
 
         newGameButton.setOnAction((ActionEvent event) -> {
             this.init();
@@ -108,5 +135,19 @@ public class GUI extends Application {
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void makeMove(int x, int y, StackPane tile, Label turnLabel) {
+        Player playerBeforeMove = gameService.getCurrentPlayer();
+        Label mark = new Label();
+        mark.setFont(Font.font(STYLESHEET_MODENA, FontWeight.BOLD, 25));
+        mark.setText(playerBeforeMove.getMark());
+        mark.setTextFill(playerBeforeMove.getMarkColor());
+        tile.getChildren().add(mark);
+
+        gameService.makeMove(x, y);
+        turnLabel.setText("Turn: " + gameService.getCurrentPlayer().getMark());
+
+        turn.set(gameService.getTurn());
     }
 }
