bc30337b17bee50cb2842f6d52ef9d1e0725250f
Toni Rämö <toni.ramo@helsinki.fi>
2021-10-01 19:17:34 +0300

    Change way number of played moves is calculated

--- tic-tac-toe/src/main/java/tictactoe/logic/GameBoard.java
+++ tic-tac-toe/src/main/java/tictactoe/logic/GameBoard.java
@@ -1,4 +1,4 @@
-package tictactoe.domain;
+package tictactoe.logic;
 
 /**
  * Tic-tac-toe game board maintaining the moves made during the game.
@@ -6,17 +6,24 @@ package tictactoe.domain;
 public class GameBoard {
 
     private Player[][] boardState;
-    
+    private Move lastMove;
+    private int playedMoves;
+
     /**
-     * Creates empty square game board with chosen size, usually checked from rule book.
+     * Creates empty square game board with chosen size, usually checked from
+     * rule book.
+     *
      * @param boardSize width of game board
      */
     public GameBoard(int boardSize) {
         this.boardState = new Player[boardSize + 1][boardSize + 1];
+        lastMove = null;
+        playedMoves = 0;
     }
 
     /**
      * Create game board based on known state.
+     *
      * @param state predefined state based on which game board is generated
      */
     public GameBoard(Player[][] state) {
@@ -25,14 +32,18 @@ public class GameBoard {
 
     /**
      * Set move on game board.
+     *
      * @param move player made move to be stored.
      */
     public void setMove(Move move) {
         boardState[move.getX()][move.getY()] = move.getPlayer();
+        lastMove = move;
+        playedMoves++;
     }
 
     /**
      * Get move from game board based on x,y coordinate.
+     *
      * @param x x-coordinate of the move to be get
      * @param y y-coordinate of the move to be get
      * @return move if found, otherwise null
@@ -47,8 +58,13 @@ public class GameBoard {
         return null;
     }
 
+    public Move getLastMove() {
+        return lastMove;
+    }
+
     /**
      * Get an array of moves based on given arrays of x,y-coordinates
+     *
      * @param x x-coordinates
      * @param y y-coordinates
      * @return array of moves if found, otherwise null;
@@ -58,14 +74,19 @@ public class GameBoard {
         Move[] moves = new Move[n];
         for (int i = 0; i < n; i++) {
             if (i < n && i < y.length) {
-                moves[i] = this.getMove(x[i], y[i]);
+                if (x[i] > 0 && x[i] < boardState.length 
+                        && y[i] > 0 && y[i] < boardState.length) {
+                    moves[i] = this.getMove(x[i], y[i]);
+                }
             }
         }
         return moves;
     }
 
     /**
-     * Gets game board size, that is width of the edges (number of tiles per edge).
+     * Gets game board size, that is width of the edges (number of tiles per
+     * edge).
+     *
      * @return width of game board edge
      */
     public int getSize() {
@@ -74,23 +95,16 @@ public class GameBoard {
 
     /**
      * Gets number of moves on board.
+     *
      * @return number of played moves
      */
     public int getNumberOfPlayedMoves() {
-        int moves = 0;
-        int n = this.getSize();
-        for (int x = 1; x <= n; x++) {
-            for (int y = 1; y <= n; y++) {
-                if (this.boardState[x][y] != null) {
-                    moves++;
-                }
-            }
-        }
-        return moves;
+        return playedMoves;
     }
 
     /**
-     * Get copy of this game board. 
+     * Get copy of this game board.
+     *
      * @return copy of this game board
      */
     public GameBoard getCopy() {
