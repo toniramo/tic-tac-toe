92e70b7c29611893cf3cd1c9a53526919ca4d4b5
Toni Rämö <toni.ramo@helsinki.fi>
2021-10-15 15:50:45 +0300

    Increase move chooser test coverage

--- tic-tac-toe/src/test/java/tictactoe/ai/AlphaBetaMoveChooserTest.java
+++ tic-tac-toe/src/test/java/tictactoe/ai/AlphaBetaMoveChooserTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoe.ai;
 
 import java.util.Random;
@@ -14,26 +9,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * AlphaBetaMoveChooser tests
+ * AlphaBetaMoveChooser tests.
  */
 public class AlphaBetaMoveChooserTest {
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void winningHorizontalMoveIsChosenV1() {
@@ -74,9 +52,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 1;
         int y0 = 1;
-        int direction = 0; // horizontal
+        int direction = 0;
         int player = 0;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 11);
 
         int[] move = AlphaBetaMoveChooser.getMove(node, new int[]{1, 1, 20, 20}, 8, 0, 5, (int) -1e9, (int) 1e9, 1);
         assertTrue(move[0] == (x0 + 4) && move[1] == y0);
@@ -87,9 +65,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 15;
         int y0 = 20;
-        int direction = 0; // horizontal
+        int direction = 0;
         int player = 0;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 13);
 
         int[] move = AlphaBetaMoveChooser.getMove(node, new int[]{1, 1, 20, 20}, 8, 0, 5, (int) -1e9, (int) 1e9, 1);
         assertTrue((move[0] == (x0 + 4) && move[1] == y0) || (move[0] == (x0 - 1) && move[1] == (y0)));
@@ -100,9 +78,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 9;
         int y0 = 6;
-        int direction = 1; // horizontal
+        int direction = 1;
         int player = 1;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 13);
 
         int[] move = AlphaBetaMoveChooser.getMove(node, new int[]{1, 1, 20, 20}, 8, 1, 5, (int) -1e9, (int) 1e9, 1);
         assertTrue((move[0] == x0 && move[1] == y0 + 4) || (move[0] == x0 && move[1] == y0 - 1));
@@ -113,9 +91,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 20;
         int y0 = 14;
-        int direction = 1; // horizontal
+        int direction = 1;
         int player = 1;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 14);
 
         int[] move = AlphaBetaMoveChooser.getMove(node, new int[]{1, 1, 20, 20}, 8, 1, 5, (int) -1e9, (int) 1e9, 1);
         assertTrue((move[0] == x0 && move[1] == y0 + 4) || (move[0] == x0 && move[1] == y0 - 1));
@@ -126,9 +104,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 10;
         int y0 = 10;
-        int direction = 2; // horizontal
+        int direction = 2;
         int player = 1;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 13);
 
         int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, 1, 5, (int) -1e9, (int) 1e9);
         assertTrue((move[0] == x0 + 4 && move[1] == y0 + 4) || (move[0] == x0 - 1 && move[1] == y0 - 1));
@@ -139,9 +117,9 @@ public class AlphaBetaMoveChooserTest {
         int[][] node = initializeNode(20);
         int x0 = 10;
         int y0 = 10;
-        int direction = 3; // horizontal
+        int direction = 3;
         int player = 1;
-        createKInRow(node, player, direction, 20, 4, x0, y0);
+        createKInRow(node, player, direction, 20, 4, x0, y0, 12);
 
         int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, 1, 5, (int) -1e9, (int) 1e9);
         assertTrue((move[0] == x0 + 4 && move[1] == y0 - 4) || (move[0] == x0 - 1 && move[1] == y0 + 1));
@@ -181,6 +159,66 @@ public class AlphaBetaMoveChooserTest {
         assertTrue(move[0] == 14 && move[1] == 10);
     }
 
+    @Test
+    public void winningHorizontalMoveIsCountered() {
+        int[][] node = initializeNode(20);
+        int x0 = 8;
+        int y0 = 11;
+        int direction = 0;
+        int player = 0;
+        int turn = 1;
+        int k = 3;
+        createKInRow(node, player, direction, 20, k, x0, y0, 27);
+
+        int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, turn, 5, (int) -1e9, (int) 1e9);
+        assertTrue((move[0] == x0 + k && move[1] == y0) || (move[0] == x0 - 1 && move[1] == y0));
+    }
+
+    @Test
+    public void winningVerticalMoveIsCountered() {
+        int[][] node = initializeNode(20);
+        int x0 = 10;
+        int y0 = 10;
+        int direction = 1;
+        int player = 0;
+        int turn = 1;
+        int k = 3;
+        createKInRow(node, player, direction, 20, k, x0, y0, 27);
+
+        int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, turn, 5, (int) -1e9, (int) 1e9);
+        assertTrue((move[0] == x0 && move[1] == y0 + k) || (move[0] == x0 && move[1] == y0 - 1));
+    }
+
+    @Test
+    public void winningDiagonalMoveIsCounteredV1() {
+        int[][] node = initializeNode(20);
+        int x0 = 5;
+        int y0 = 5;
+        int direction = 2;
+        int player = 0;
+        int turn = 1;
+        int k = 3;
+        createKInRow(node, player, direction, 20, k, x0, y0, 27);
+
+        int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, turn, 5, (int) -1e9, (int) 1e9);
+        assertTrue((move[0] == x0 + k && move[1] == y0 + k) || (move[0] == x0 - 1 && move[1] == y0 - 1));
+    }
+
+    @Test
+    public void winningDiagonalMoveIsCounteredV2() {
+        int[][] node = initializeNode(20);
+        int x0 = 5;
+        int y0 = 13;
+        int direction = 3;
+        int player = 1;
+        int turn = 0;
+        int k = 3;
+        createKInRow(node, player, direction, 20, k, x0, y0, 27);
+
+        int[] move = AlphaBetaMoveChooser.getMoveWithOptimizedSearchDepth(node, new int[]{1, 1, 20, 20}, 8, turn, 5, (int) -1e9, (int) 1e9);
+        assertTrue((move[0] == x0 + k && move[1] == y0 - k) || (move[0] == x0 - 1 && move[1] == y0 + 1));
+    }
+
     private int[][] initializeNode(int n) {
         int[][] node = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
@@ -195,18 +233,13 @@ public class AlphaBetaMoveChooserTest {
      * Creates test node in which one player is about to win and another has
      * marks in various corners of board.
      */
-    private void createKInRow(int[][] node, int player, int direction, int n, int k, int x0, int y0) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                node[i][j] = -1;
-            }
-        }
+    private void createKInRow(int[][] node, int player, int direction, int n, int k, int x0, int y0, int seed) {
         for (int i = 0; i < k; i++) {
             int x = getNextX(x0, direction, i);
             int y = getNextY(y0, direction, i);
             node[x][y] = player;
         }
-        generateRandomMoves(node, (player + 1) % 2, n, k - 1, 13);
+        generateRandomMoves(node, (player + 1) % 2, n, k - 1, seed);
     }
 
     /**
@@ -214,7 +247,7 @@ public class AlphaBetaMoveChooserTest {
      *
      * @param x0 x-coordinate of starting point
      * @param direction direction in which row is formed ( 0: horizontal / right
-     * to left 1: vertical / down to tup 2: diagonal / down left to up right 3:
+     * to left 1: vertical / down to up 2: diagonal / down left to up right 3:
      * diagonal / up left to down right)
      * @param i index in row
      * @return next x-coordinate
@@ -231,7 +264,7 @@ public class AlphaBetaMoveChooserTest {
      *
      * @param y0 y-coordinate of starting point
      * @param direction direction in which row is formed ( 0: horizontal / right
-     * to left 1: vertical / down to tup 2: diagonal / down left to up right 3:
+     * to left 1: vertical / down to up 2: diagonal / down left to up right 3:
      * diagonal / up left to down right)
      * @param i index in row
      * @return next y-coordinate
@@ -248,11 +281,11 @@ public class AlphaBetaMoveChooserTest {
 
     /**
      * Generates m random moves on board (uses random seed to get repeatable
-     * resuts).
+     * results).
      *
      * @param node status of game board
      * @param player player whose marks are located on board
-     * @param n board size (width / lenght)
+     * @param n board size (width / length)
      * @param m number generated moves
      */
     private void generateRandomMoves(int[][] node, int player, int n, int m, int seed) {
